4ccff011579128e992c32916ad3113620243f487
Mike Mike <mikesbcmike@gmail.com>
2021-02-22 13:13:32 -0600

    I added Jsdoc and a function for readbility

--- js/time.js
+++ js/time.js
@@ -1,13 +1,32 @@
+
+/** @file - this webpage gives greetings
+ * @author - Mike Mike
+ */
+
+/** @var {method} today
+ * @var {method} hourNow
+ * @var {string} greeting
+ */
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
-if(hourNow > 10) {
-  greeting = "Good Evening";
-} else if(hour > 12){
-  greeting= "Good Afternoon";
-} else if (hour > 0 ) {
-  greeting = "Good morning";
-}else {
-  greeting="welcome";
+
+
+/** @function timeOfDay
+ * @param {string} greeting - This gives greetings based of time
+ */
+function timeOfDay(greeting){
+if (hourNow > 18) {
+    greeting = 'Good evening!';
+} else if (hourNow > 12) {
+   greeting = 'Good afternoon!';
+} else if (hourNow > 0) {
+    greeting = 'Good morning!';
+} else {
+  greeting = 'Welcome!';
 }
-document.querySelector('h3').innerHTML= `${greeting}`;
+return greeting;
+
+}
+/** @description - this will print greeting */
+document.write(`<h3>${timeOfDay()}</h3>`);
